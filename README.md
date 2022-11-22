c6f4358c9c1ad5c31ddfb7e0d914273c59fc23cb
Vinicius Brandalize <brandalize.vinicius@gmail.com>
2022-11-22 10:26:45 -0300

    torus

--- README.md
+++ README.md
@@ -1,14 +1,40 @@
-# threejs-textures
-Textures in Three.js.
+<h1 align='center'>UNOESC - Computação Gráfica - A1.3</h1>
 
-## Setup
-Download [Node.js](https://nodejs.org/).
-Run this followed commands:
+## 📝 Demandas solicitadas:   
+Implementar uma cena em Three.js ilustrando um palco com:
+
+a) um ponto de luz fixo na parte superior da cena, simulando uma lâmpada que emite luz para todas as direções;
+
+b) três pontos de luz fixos, simulando canhões que emitem luz ao longo de um cone que aumenta de tamanho à medida que se afasta, direcionados para um objeto animado em cima do palco.
+
+O objeto animado pode ser modelado com qualquer recurso do Three.js ou importado de um arquivo externo.
+
+Todas as luzes devem projetar sombras para simular um ambiente físico real.
+
+<br>
+
+## ⚙️ Configurações:
+Realizar o download do [Node.js](https://nodejs.org/en/download/).
+
+No terminal do Visual Studio Code, navegar até a pasta do projeto e executar os comnados abaixo
 
 ``` bash
-# Install dependencies (only the first time)
+# Instalar as dependencias (apenas na primeira vez)
 npm install
 
-# Build for production in the dist/ directory
+# Build para finalização
 npm run build
 ```
+
+Após, abra no seu navegador a url abaixo
+    
+``` bash
+http://127.0.0.1:5500/dist/index.html
+```
+
+<br>
+
+## 🤵 Alunos:
+
+  - Vinicius Brandalize 
+  - Gabriel Rocha
--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 const loader = new THREE.TextureLoader()
 const ground = loader.load('textures/ground.jpg')
 const textPalco = loader.load('textures/ground5.jpg')
+const height = loader.load('textures/4.png')
 
 const renderer = new THREE.WebGLRenderer();
 renderer.shadowMap.enabled = true;
@@ -19,17 +20,16 @@ const camera = new THREE.PerspectiveCamera(
     1,
     500
 );
-camera.position.set(0, -30, 40);
+camera.position.set(0, 25, 40);
 camera.lookAt( 0, 0, 0 );
 
 const scene = new THREE.Scene();
 
 //Create a DirectionalLight and turn on shadows for the light
-const light = new THREE.PointLight( 0xffffff, 0.01, 40 );
-light.position.set( 0, 0, 30 ); //default; light shining from top
-light.rotateY(-90);
+const light = new THREE.PointLight( 0xffffff, 0.5, 40 );
+light.position.set( 0, 30, 0 ); //default; light shining from top
 light.castShadow = true; // default false
-//scene.add( light );
+scene.add( light );
 
 //Set up shadow properties for the light
 light.shadow.mapSize.width = 512; // default
@@ -40,7 +40,7 @@ light.shadow.camera.far = 500; // default
 
 //Create a SpotLight and turn on shadows for the light
 const spotlight = new THREE.SpotLight( 0xffffff, 0.5);
-spotlight.position.set( -32, -32, 20 );
+spotlight.position.set( -32, 32, 20 );
 spotlight.castShadow = true; // default false
 
 //Set up shadow properties for the light
@@ -55,7 +55,7 @@ scene.add( spotlight );
 
 //Create a SpotLight and turn on shadows for the light
 const spotlight2 = new THREE.SpotLight( 0xffffff, 0.5);
-spotlight2.position.set( 32, -32, 20 );
+spotlight2.position.set( 32, 32, 20 );
 spotlight2.castShadow = true; // default false
 
 //Set up shadow properties for the light
@@ -69,7 +69,7 @@ scene.add( spotlight2 );
 
 //Create a SpotLight and turn on shadows for the light
 const spotlight3 = new THREE.SpotLight( 0xffffff , 0.5);
-spotlight3.position.set( 0, -32, 20 );
+spotlight3.position.set( 0, 32, 20 );
 spotlight3.castShadow = true; // default false
 
 //Set up shadow properties for the light
@@ -84,22 +84,21 @@ scene.add( spotlight3 );
 
 
 //Create a plane that receives shadows (but does not cast them)
-const planeGeometry = new THREE.PlaneGeometry( 60, 60, 32, 32 );
+const planeGeometry = new THREE.BoxGeometry( 60, 0.5, 60 );
 const planeMaterial = new THREE.MeshStandardMaterial( { 
     map: ground,
     //color: 0xffffff,
     //emissive:0x000000,
     //metalness: 0.5,
-    //roughness: 0
+    roughness: 1,
+    //displacementMap: height,
+    //displacementScale: 1
 } )
 const plane = new THREE.Mesh( planeGeometry, planeMaterial );
 plane.receiveShadow = true;
-scene.add( plane );
-plane.position.y = 0
-plane.position.x = 0
 scene.add(plane)
 
-const palcoGeometry = new THREE.BoxGeometry(20, 13,10 );
+const palcoGeometry = new THREE.BoxGeometry(20, 10, 20 );
 const palcoMaterial = new THREE.MeshStandardMaterial( { 
     map: textPalco,
     //color: 0x049ef4,
@@ -110,36 +109,50 @@ const palcoMaterial = new THREE.MeshStandardMaterial( {
 const palco = new THREE.Mesh( palcoGeometry, palcoMaterial );
 palco.castShadow = true; //default is false
 palco.receiveShadow = true; //default
-palco.position.z = 0
-palco.position.y = -1
 scene.add( palco );
 
 
 //Create a box that cast shadows (but does not receive them)
-const boxGeometry = new THREE.BoxGeometry(4, 4,4 );
-const boxMaterial = new THREE.MeshStandardMaterial( { 
+// const boxGeometry = new THREE.BoxGeometry(4, 4,4 );
+// const boxMaterial = new THREE.MeshStandardMaterial( { 
+//     color: 0x049ef4,
+//     emissive:0x000000,
+//     metalness: 0.5,
+//     roughness: 0
+// } );
+// const box = new THREE.Mesh( boxGeometry, boxMaterial );
+// box.castShadow = true; //default is false
+// box.receiveShadow = false; //default
+// box.position.set(0,10,0);
+// scene.add( box );
+
+
+const geometry = new THREE.TorusKnotGeometry( 10, 3, 300, 20,2,3 );
+const material = new THREE.MeshBasicMaterial( { 
     color: 0x049ef4,
     emissive:0x000000,
-    metalness: 0.5,
+    metalness: 1,
     roughness: 0
-} );
-const box = new THREE.Mesh( boxGeometry, boxMaterial );
-box.castShadow = true; //default is false
-box.receiveShadow = false; //default
-box.position.z = 10
-scene.add( box );
+});
+const torusKnot = new THREE.Mesh( geometry, material );
+torusKnot.castShadow = true; //default is false
+torusKnot.receiveShadow = false; //default
+torusKnot.position.set(0,12,0);
+torusKnot.scale.set(0.35,0.35,0.35)
+scene.add( torusKnot );
 
-const loader2 = new GLTFLoader();
-loader2.load('textures/Ankit.glb', function(glb){
-    console.log(glb);
-    const root = glb.scene;
-    root.position.set(0,0,5);
-	root.scale.set(5,5,5);
-    root.rotateX(90);
-    root.castShadow = true;
-    root.receiveShadow = true;
-    //scene.add(root);
-})
+
+// const loader2 = new GLTFLoader();
+// loader2.load('textures/Ankit.glb', function(glb){
+//     console.log(glb);
+//     const root = glb.scene;
+//     root.position.set(0,0,5);
+//     root.scale.set(5,5,5);
+//     root.rotateX(90);
+//     root.castShadow = true;
+//     root.receiveShadow = true;
+//     //scene.add(root);
+// })
 
 
 //Create a helper for the shadow camera (optional)
@@ -168,9 +181,9 @@ function animate() {
 
     //loader2.root.rotateX(0.1)
 
-    box.rotateX(0.01)
-    box.rotateY(0.01)
-    box.rotateZ(0.01)
+    torusKnot.rotateX(0.01)
+    torusKnot.rotateY(0.01)
+    torusKnot.rotateZ(0.01)
 
 	controls.update();
 	renderer.render( scene, camera );
